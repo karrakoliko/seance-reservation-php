1d2e4fe9e44550e37c90d551e7787d64d4b5ac16
karrakoliko <github-public@karrakoliko.fastmail.com>
2023-08-23 17:04:19 +1000

    Project sketeton added - composer configured - phpunit configured

---
+++ tests/Unit/DummyUnitTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Karrakoliko\SeanceReservation\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class DummyUnitTest extends TestCase
+{
+    /**
+     * Dummy test to ensure phpunit configured properly
+     * @return void
+     */
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+
+}
